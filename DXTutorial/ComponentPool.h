1efe1353af022a1627afed0ee8f6c3dfd7f1bd11
Zach <zach303@msn.com>
2019-02-04 13:52:21 -0800

    removed reliance on component structure

--- DXTutorial/ComponentPool.h
+++ DXTutorial/ComponentPool.h
@@ -31,54 +31,57 @@ public:
 	bool StartUp(U32 poolSize)
 	{
 		m_pool.reserve(poolSize);
-		m_remap.reserve(poolSize);
+		m_remapToPool.reserve(poolSize);
+		m_remapToId.reserve(poolSize);
 		m_entityMap.reserve(poolSize);
 		return true;
 	}
 
 
-	T& CreateComponent(Entity e)
+	U64 CreateComponent(Entity e)
 	{
+		U64 id;
 		U32 idx;
 		U32 gen;
-		U64 remap;
+		U64 remapId;
 
 		if (m_freedIds.size() > m_minFree)
 		{
 			// reuse an id
-			m_next.id = m_freedIds.front();
+			id = m_freedIds.front();
 			m_freedIds.pop_front();
 
 			idx = Idx(m_next.id);
 			gen = Gen(m_next.id);
 
 			// generate remap value
-			remap = Combine(m_numActive, gen);
-			m_remap[idx] = remap;
+			remapId = Combine(m_numActive, gen);
+			m_remapToPool[idx] = remapId;
 		}
 		else
 		{
-			idx = m_remap.size();
+			idx = m_remapToPool.size();
 			gen = 0;
 
 			// create new id and set generation to 0
-			m_next.id = Combine(idx, gen);
+			id = Combine(idx, gen);
 
 			// generate remap value
-			remap = Combine(m_numActive, gen);
-			m_remap.push_back(remap);
+			remapId = Combine(m_numActive, gen);
+			m_remapToPool.push_back(remapId);
 		}
 
 
 		// add component to pool
 		m_pool.push_back(m_next);
+		m_remapToId.push_back(id);
 		m_numActive++;
 
 		// set entity map
-		m_entityMap.emplace(e.id, remap);
+		m_entityMap.emplace(e.id, remapId);
 
 		// return component
-		return m_next;
+		return id;
 	}
 
 
@@ -98,7 +101,7 @@ public:
 
 	T& GetComponentById(U64 id)
 	{
-		U64 remap = m_remap[Idx(id)];
+		U64 remap = m_remapToPool[Idx(id)];
 
 		if (Gen(id) == Gen(remap))
 		{
@@ -132,26 +135,29 @@ public:
 	{
 		U32 idxToRemap = Idx(id);
 		U32 gen = Gen(id);
-		U64 remapId = m_remap[idxToRemap];
+		U64 remapId = m_remapToPool[idxToRemap];
 		U32 idxToPool = Idx(remapId);
 		U32 remapGen = Gen(remapId);
 
 		if (gen == remapGen)
 		{
-			// swap back component with obsolete component
+			// swap back component with obsolete component and reduce pool
 			T back = m_pool.back();
 			m_pool[idxToPool] = back;
-
-			// reduce pool
 			m_pool.pop_back();
 			m_numActive--;
 
-			// update remap of swapped component to new pool idx
-			m_remap[Idx(back.id)] = Combine(idxToPool, Gen(back.id));
+			// update pool idx to id remap
+			U64 swappedId = m_remapToId.back();
+			m_remapToId[idxToPool] = back;
+			m_remapToId.pop_back();
 
+			// update id to pool idx remap
+			m_remapToPool[Idx(swappedId)] = Combine(idxToPool, Gen(swappedId));
+			
 			// increment gen on remap of destroyed component to invalidate stale ids
 			gen++;
-			m_remap[idxToRemap] = Combine(0, gen);
+			m_remapToPool[idxToRemap] = Combine(0, gen);
 
 			// add id with incremented gen to freed ids
 			m_freedIds.push_back(Combine(idxToRemap, gen));
@@ -180,7 +186,8 @@ private:
 
 private:
 	std::vector<T> m_pool;
-	std::vector<U64> m_remap;
+	std::vector<U64> m_remapToPool;
+	std::vector<U64> m_remapToId;
 	std::deque<U32> m_freedIds;
 	std::unordered_map<U64, U64> m_entityMap;
 
