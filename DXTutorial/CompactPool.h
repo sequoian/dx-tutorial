9ea182a8e17734f7b96f7350dda9da1fb9cfa849
Zach <zach303@msn.com>
2019-02-12 19:27:52 -0800

    put template code back into header files and added const functions to pool and system

--- DXTutorial/CompactPool.h
+++ DXTutorial/CompactPool.h
@@ -8,25 +8,165 @@ template <class T>
 class CompactPool
 {
 public:
-	bool StartUp(U32 poolSize);
-	U64 CreateObject();
-	bool GetObject(U64 handle, T& object);
-	void DestroyObject(U64 handle);
-	U32 Size() const;
-	T& operator[] (I32);
+	typedef typename std::vector<T>::iterator iterator;
+	typedef typename std::vector<T>::const_iterator const_iterator;
+
+public:
+	inline bool StartUp(U32 poolSize)
+	{
+		m_pool.reserve(poolSize);
+		m_remapToPool.reserve(poolSize);
+		m_remapToHandle.reserve(poolSize);
+		return true;
+	}
+
+
+	inline U64 CreateObject()
+	{
+		U64 handle;
+		U32 idx;
+		U32 gen;
+		U64 remapHandle;
+
+		if (m_freedHandles.size() > m_minFree)
+		{
+			// reuse a handle
+			handle = m_freedHandles.front();
+			m_freedHandles.pop_front();
+
+			idx = Idx(handle);
+			gen = Gen(handle);
+
+			// generate remap value
+			remapHandle = Combine(m_numActive, gen);
+			m_remapToPool[idx] = remapHandle;
+		}
+		else
+		{
+			// create new handle and set generation to 0
+			idx = m_remapToPool.size();
+			gen = 0;
+			handle = Combine(idx, gen);
+
+			// generate remap value
+			remapHandle = Combine(m_numActive, gen);
+			m_remapToPool.push_back(remapHandle);
+		}
+
+		// add component to pool
+		m_pool.push_back(m_next);
+		m_remapToHandle.push_back(handle);
+		m_numActive++;
+
+		// return component handle
+		return handle;
+	}
+
+
+	inline T* GetObject(U64 handle)
+	{
+		U64 remap = m_remapToPool[Idx(handle)];
+
+		if (Gen(handle) == Gen(remap))
+		{
+			return &m_pool[Idx(remap)];
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+
+	inline const T* GetObjectConst(U64 handle) const
+	{
+		return GetObject(handle);
+	}
+
+
+	inline void DestroyObject(U64 handle)
+	{
+		U32 idxToRemap = Idx(handle);
+		U32 gen = Gen(handle);
+		U64 remapHandle = m_remapToPool[idxToRemap];
+		U32 idxToPool = Idx(remapHandle);
+		U32 remapGen = Gen(remapHandle);
+
+		if (gen == remapGen)
+		{
+			// swap back component with obsolete component and reduce pool
+			T back = m_pool.back();
+			m_pool[idxToPool] = back;
+			m_pool.pop_back();
+			m_numActive--;
+
+			// update pool idx to handle remap
+			U64 swappedHandle = m_remapToHandle.back();
+			m_remapToHandle[idxToPool] = back;
+			m_remapToHandle.pop_back();
+
+			// update handle to pool idx remap
+			m_remapToPool[Idx(swappedHandle)] = Combine(idxToPool, Gen(swappedHandle));
+
+			// increment gen on remap of destroyed component to invalidate stale handles
+			gen++;
+			m_remapToPool[idxToRemap] = Combine(0, gen);
+
+			// add handle with incremented gen to freed handles
+			m_freedHandles.push_back(Combine(idxToRemap, gen));
+		}
+	}
+
+
+	inline U32 Size() const
+	{
+		return m_numActive;
+	}
+
+
+	inline T* operator[] (I32 idx)
+	{
+		return m_pool[idx];
+	}
+
+
+	iterator Begin()
+	{
+		return m_pool.begin();
+	}
+
+
+	iterator End()
+	{
+		return m_pool.end();
+	}
+
+
+	const_iterator CBegin()
+	{
+		return m_pool.cbegin();
+	}
+
+
+	const_iterator CEnd()
+	{
+		return m_pool.cend();
+	}
+
 
 private:
-	U32 Gen(U64 handle) const
+	inline U32 Gen(U64 handle) const
 	{
 		return handle & m_bitMask;
 	}
 
-	U32 Idx(U64 handle) const
+
+	inline U32 Idx(U64 handle) const
 	{
 		return (handle >> m_bitShift) & m_bitMask;
 	}
 
-	U64 Combine(U32 idx, U32 gen) const
+
+	inline U64 Combine(U32 idx, U32 gen) const
 	{
 		return ((U64)gen << m_bitShift) | idx;
 	}
--- DXTutorial/ComponentSystem.h
+++ DXTutorial/ComponentSystem.h
@@ -12,12 +12,73 @@ public:
 	// execute system
 	virtual void Execute() = 0;
 
-	virtual bool StartUp(U32);
-	virtual U64 CreateComponent(Entity e);
-	virtual T& FindComponent(Entity e);
-	virtual inline T& GetComponentByHandle(U64 handle);
-	virtual void DestroyComponent(Entity e);
-	virtual void DestroyComponentByHandle(U64 handle);
+	virtual inline bool StartUp(U32)
+	{
+		m_pool.StartUp(numComponents);
+	}
+
+
+	virtual inline U64 CreateComponent(Entity e)
+	{
+		U64 handle = m_pool.CreateObject();
+
+		// set entity map
+		m_entityMap.emplace(e.id, handle);
+
+		return handle;
+	}
+
+
+	virtual inline T* FindComponent(Entity e)
+	{
+		auto itr = m_entityMap.find(e.id);
+		if (itr == m_entityMap.end())
+		{
+			return nullptr;
+		}
+		else
+		{
+			return GetComponentByHandle(itr->second);
+		}
+	}
+
+
+	virtual inline const T* FindComponentConst(Entity e) const
+	{
+		return FindComponent(e);
+	}
+
+
+	virtual inline T* GetComponentByHandle(U64 handle)
+	{
+		return m_pool.GetObject(handle);
+	}
+
+
+	virtual inline const T* GetComponentByHandleConst(U64 handle) const
+	{
+		return GetComponentByHandle(handle);
+	}
+
+
+	virtual inline void DestroyComponent(Entity e)
+	{
+		auto itr = m_entityMap.find(e.id);
+		if (itr != m_entityMap.end())
+		{
+			DestroyComponentByHandle(itr->second);
+
+			// remove entity from map
+			m_entityMap.erase(itr);
+		}
+	}
+
+
+	virtual inline void DestroyComponentByHandle(U64 handle)
+	{
+		m_pool.DestroyObject(handle);
+	}
+
 
 private:
 	CompactPool<T> m_pool;
