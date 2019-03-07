e72c235147a5691d9be7518feb859342291796a5
Zach <zach303@msn.com>
2019-03-07 13:32:49 -0800

    bundle srv with texture for simplicity

--- DXTutorial/Texture.h
+++ DXTutorial/Texture.h
@@ -12,9 +12,16 @@ public:
 
 	void ShutDown()
 	{
+		if (m_srv != nullptr)
+		{
+			m_srv->Release();
+			m_srv = nullptr;
+		}
+
 		if (m_texture != nullptr)
 		{
 			m_texture->Release();
+			m_texture = nullptr;
 		}
 	}
 
@@ -26,15 +33,27 @@ public:
 			return false;
 		}
 
+		m_srv = graphics.CreateShaderResource(m_texture);
+		if (!m_srv)
+		{
+			return false;
+		}
+
 		return true;
 	}
 
-	ID3D11Resource* Get()
+	inline ID3D11Resource* Get()
 	{
 		return m_texture;
+	}
+
+	inline ID3D11ShaderResourceView* GetResourceView()
+	{
+		return m_srv;
 	}
 
 
 private:
 	ID3D11Resource* m_texture = nullptr;
+	ID3D11ShaderResourceView* m_srv = nullptr;
 };
