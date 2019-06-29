2603eaa1ffa0e18793aaaef3760f31a39ff38a56
Zach <zach303@msn.com>
2019-06-28 19:45:26 -0700

    do not apply gravity while grounded

--- Source/KinematicGravitySystem.h
+++ Source/KinematicGravitySystem.h
@@ -4,26 +4,29 @@
 #include "TransformSystem.h"
 #include "MathUtility.h"
 #include "VelocitySystem.h"
+#include "LegCastSystem.h"
 
 struct KinematicGravityComponent
 {
 	U64 hTransform;
 	U64 hVelocity;
+	U64 hLegCast;
 	float gravity;
 };
 
 class KinematicGravitySystem : public ComponentSystem<KinematicGravityComponent>
 {
 public:
-	bool StartUp(U32 numComponents, EntityManager& em, TransformSystem& transformSystem, VelocitySystem& velocity)
+	bool StartUp(U32 numComponents, EntityManager& em, TransformSystem& transformSystem, VelocitySystem& velocity, LegCastSystem& lcs)
 	{
 		Parent::StartUp(numComponents, em);
 		m_transformSystem = &transformSystem;
 		m_velocitySystem = &velocity;
+		m_legCastSystem = &lcs;
 		return true;
 	}
 
-	U64 CreateComponent(Entity e, U64 hTransform, U64 hVelocity, float gravity)
+	U64 CreateComponent(Entity e, U64 hTransform, U64 hVelocity, U64 hLegCast, float gravity)
 	{
 		U64 handle = Parent::CreateComponent(e);
 		KinematicGravityComponent* comp = GetComponentByHandle(handle);
@@ -31,6 +34,7 @@ public:
 		comp->hTransform = hTransform;
 		comp->hVelocity = hVelocity;
 		comp->gravity = gravity;
+		comp->hLegCast = hLegCast;
 
 		return handle;
 	}
@@ -43,12 +47,17 @@ public:
 			// still need transform?
 			//TransformComponent* transform = m_transformSystem->GetComponentByHandle(comp->hTransform);
 			VelocityComponent* velocity = m_velocitySystem->GetComponentByHandle(comp->hVelocity);
+			LegCastComponent* legCast = m_legCastSystem->GetComponentByHandle(comp->hLegCast);
 
-			velocity->velocity += Vector3(0, -comp->gravity * deltaTime, 0);
+			if (!legCast->grounded)
+			{
+				velocity->velocity += Vector3(0, -comp->gravity * deltaTime, 0);
+			}
 		}
 	}
 
 protected:
 	TransformSystem* m_transformSystem;
 	VelocitySystem* m_velocitySystem;
+	LegCastSystem* m_legCastSystem;
 };
