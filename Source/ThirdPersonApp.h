e4897012289bb168df8a723471a20874748a9520
Zach <zach303@msn.com>
2019-07-04 19:49:55 -0700

    checkpoints and kill triggers added

--- Source/ThirdPersonApp.h
+++ Source/ThirdPersonApp.h
@@ -41,7 +41,7 @@
 #include "PistonSystem.h"
 
 // preprocessor directives
-#define SHOW_TRIGGERS true;
+#define SHOW_TRIGGERS false;
 
 struct ModelConstants
 {
@@ -255,7 +255,7 @@ public:
 
 		// default spawn
 		e = m_entityManager.CreateEntity();
-		m_spawnSystem.CreateComponent(e, Vector3(-18, 9, 50), Quaternion(0, 180.0_rad, 0));
+		m_spawnSystem.CreateComponent(e, Vector3(40, 22, -24), Quaternion(0, 180.0_rad, 0));
 
 		// player
 		e = m_entityManager.CreateEntity();
@@ -282,12 +282,15 @@ public:
 		MakePlatform(Vector3(0, 2, 0), Quaternion(), Vector3(15, 1, 1), m_sand);
 		MakePlatform(Vector3(0, 3.4, 22), Quaternion(65.0_rad, 0, 0), Vector3(5, 8, 1), m_sand);
 		MakePlatform(Vector3(0, 6.7, 33.5), Quaternion(), Vector3(8, 1, 5), m_sand);
+		MakeCheckpoint(Vector3(0, 9, 33.5), Quaternion(), Vector3(0, 9, 33.5), Quaternion(), Vector3(7, 1, 4));
 		MakePlatform(Vector3(0, 7, 50), Quaternion(), Vector3(5, 1, 5), m_sand);
 		MakePlatform(Vector3(18, 7, 50), Quaternion(), Vector3(5, 1, 5), m_sand);
 		MakePlatform(Vector3(-18, 7, 50), Quaternion(), Vector3(5, 1, 5), m_sand);
+		MakeCheckpoint(Vector3(-18, 10, 50), Quaternion(), Vector3(-18, 10, 50), Quaternion(), Vector3(4, 1, 4));
 		MakePlatform(Vector3(-38, 7, 50), Quaternion(), Vector3(5, 1, 5), m_sand);
 		MakePiston(Vector3(-28, 12, 50), Vector3(-28, 7, 50), Vector3(0.5, 0.5, 5), 1.25, 0, 1.25, 0);
 		MakePlatform(Vector3(-38, 8.25, 44), Quaternion(), Vector3(5, 0.25, 1), m_sand);
+		MakeKillTrigger(Vector3(0, -10, 0), Vector3(80, 1, 80));
 
 		// stairs
 		MakePlatform(Vector3(-38, 8.75, 43), Quaternion(), Vector3(5, 0.25, 1), m_sand);
@@ -295,39 +298,51 @@ public:
 		MakePlatform(Vector3(-38, 9.75, 41), Quaternion(), Vector3(5, 0.25, 1), m_sand);
 		MakePlatform(Vector3(-38, 10.25, 40), Quaternion(), Vector3(5, 0.25, 1), m_sand);
 		MakePlatform(Vector3(-38, 10.75, 34.5), Quaternion(), Vector3(5, 0.25, 5), m_sand);
+		MakeCheckpoint(Vector3(-38, 12.75, 34.5), Quaternion(), Vector3(-38, 12.75, 34.5), Quaternion(), Vector3(4, 1, 4));
 
 		// climb up
 		MakePlatform(Vector3(-38, 12.5, 22), Quaternion(), Vector3(3, 0.5, 3), m_sand);
 		MakePlatform(Vector3(-48, 15, 15), Quaternion(), Vector3(3, 0.5, 3), m_sand);
+		MakeCheckpoint(Vector3(-48, 17, 15), Quaternion(), Vector3(-48, 17, 15), Quaternion(), Vector3(2, 1, 2));
 		MakePlatform(Vector3(-40, 18.5, 10), Quaternion(), Vector3(3, 0.5, 3), m_sand);
+		MakeCheckpoint(Vector3(-40, 20.5, 10), Quaternion(), Vector3(-40, 20.5, 10), Quaternion(), Vector3(2, 1, 2));
 
 		// obstacle course
 		MakePlatform(Vector3(-45, 20, -20), Quaternion(), Vector3(5, 0.5, 25), m_sand);
 		// obstacle 1
 		MakePiston(Vector3(-45, 23.5, -4), Vector3(-45, 30, -4), Vector3(4.5, 3, 2), 1.2, 0.25, 0.25, 1);
+		MakeCheckpoint(Vector3(-45, 22, -11.5), Quaternion(), Vector3(-45, 22, -11.5), Quaternion(), Vector3(5, 3, 0.5));
 		// obstacle 2
 		MakePiston(Vector3(-42.5, 23, -19), Vector3(-37.5, 23, -19), Vector3(2.5), .9, 0.25, 0.25, 1);
 		MakePiston(Vector3(-47.5, 23, -19), Vector3(-52.5, 23, -19), Vector3(2.5), .9, 0.25, 0.25, 1);
+		MakeCheckpoint(Vector3(-45, 22, -26), Quaternion(), Vector3(-45, 22, -26), Quaternion(), Vector3(5, 3, 0.5));
 		//obstacle 3
 		MakePiston(Vector3(-45, 23.5, -33), Vector3(-45, 17.5, -33), Vector3(4.5, 3, 0.5), 0.75, 0, 0.5, 0);
 
 		// obstacles with jumping
 		MakePlatform(Vector3(-37, 20, -40), Quaternion(), Vector3(3, 0.5, 5), m_sand);
+		MakeCheckpoint(Vector3(-37, 22, -40), Quaternion(), Vector3(-37, 22, -40), Quaternion(), Vector3(2, 1, 4));
 		MakePropeller(Vector3(-30, 20, -40), Quaternion(), Vector3(0.5, 0.5, 7), Vector3(1, 0, 0), 3);
 		MakePlatform(Vector3(-25, 20, -40), Quaternion(), Vector3(3, 1, 5), m_sand);
+		MakeCheckpoint(Vector3(-25, 22, -40), Quaternion(), Vector3(-25, 22, -40), Quaternion(), Vector3(2, 1, 4));
 		MakePropeller(Vector3(-17, 20, -40), Quaternion(), Vector3(0.5, 7, 0.5), Vector3(1, 0, 0), -2);
 		MakePropeller(Vector3(-17, 20, -40), Quaternion(), Vector3(0.5, 0.5, 7), Vector3(1, 0, 0), -2);
 		MakePlatform(Vector3(-10, 20, -40), Quaternion(), Vector3(3, 1, 5), m_sand);
+		MakeCheckpoint(Vector3(-10, 22, -40), Quaternion(), Vector3(-10, 22, -40), Quaternion(), Vector3(2, 1, 4));
 		MakePropeller(Vector3(-1, 20, -40), Quaternion(), Vector3(0.5, 7, 0.5), Vector3(1, 0, 0), -2);
 		MakePropeller(Vector3(-4, 20, -40), Quaternion(), Vector3(0.5, 7, 0.5), Vector3(1, 0, 0), 2);
 		MakePlatform(Vector3(5, 20, -40), Quaternion(), Vector3(3, 1, 5), m_sand);
+		MakeCheckpoint(Vector3(5, 22, -40), Quaternion(), Vector3(5, 22, -40), Quaternion(), Vector3(2, 1, 4));
 		MakePropeller(Vector3(12, 20, -40), Quaternion(), Vector3(0.5, 0.5, 7), Vector3(1, 0, 0), -4);
 		MakePlatform(Vector3(20, 20, -40), Quaternion(), Vector3(3, 1, 3), m_sand);
+		MakeCheckpoint(Vector3(20, 22, -40), Quaternion(), Vector3(20, 22, -40), Quaternion(), Vector3(2, 1, 2));
 		MakePlatform(Vector3(40, 20, -40), Quaternion(), Vector3(8, 1, 8), m_sand);
 		MakePropeller(Vector3(40, 22, -40), Quaternion(), Vector3(10, 0.5, 0.5), Vector3(0, 1, 0), -2);
 		MakePropeller(Vector3(40, 22, -40), Quaternion(), Vector3(0.5, 0.5, 10), Vector3(0, 1, 0), -2);
 		MakePlatform(Vector3(40, 20, -24), Quaternion(), Vector3(4, 1, 8), m_sand);
+		MakeCheckpoint(Vector3(40, 22, -24), Quaternion(), Vector3(40, 22, -24), Quaternion(), Vector3(3, 1, 5));
 		MakePlatform(Vector3(40, 20, -7), Quaternion(), Vector3(9, 1, 9), m_sand);
+		MakeKillTrigger(Vector3(0, 15, -40), Vector3(50, 1, 20));
 		
 
 		// doorway
@@ -469,6 +484,7 @@ private:
 	Entity MakeCheckpoint(XMVECTOR spawnPos, XMVECTOR spawnRot, XMVECTOR triggerPos, XMVECTOR triggerRot, XMVECTOR triggerScale);
 	Entity MakePropeller(XMVECTOR pos, XMVECTOR rot, XMVECTOR scale, XMVECTOR axis, float speed);
 	Entity MakePiston(XMVECTOR startPos, XMVECTOR endPos, XMVECTOR scale, float timeToEnd, float timeAtEnd, float timeToStart, float timeAtStart);
+	Entity MakeKillTrigger(XMVECTOR pos, XMVECTOR scale);
 };
 
 
@@ -555,6 +571,23 @@ Entity ThirdPersonApp::MakePiston(XMVECTOR startPos, XMVECTOR endPos, XMVECTOR s
 	m_kinematicRBSystem.CreateComponent(e, hTransform, hRigidBody);
 	m_deadlyTouchSystem.CreateComponent(e);
 	m_pistonSystem.CreateComponent(e, hTransform, startPos, endPos, timeToEnd, timeToStart, timeAtEnd, timeAtStart);
+
+	return e;
+}
+
+Entity ThirdPersonApp::MakeKillTrigger(XMVECTOR pos, XMVECTOR scale)
+{
+	Entity e = m_entityManager.CreateEntity();
+	U64 hTransform = m_transformSystem.CreateComponent(e, pos, Quaternion(), scale);
+	TransformComponent* transform = m_transformSystem.GetComponentByHandle(hTransform);
+	#if SHOW_TRIGGERS == true
+		m_meshSystem.CreateComponent(e, hTransform, m_cube, m_stone);
+	#endif
+	ColliderPtr collider = m_physics.CreateCollisionBox(1, 1, 1);
+	collider.SetScale(transform->scale);
+	RigidBody rb = m_physics.CreateStaticRigidBody(e, collider, transform->position, transform->rotation, true);
+	m_rigidBodySystem.CreateComponent(e, rb);
+	m_deadlyTouchSystem.CreateComponent(e);
 
 	return e;
 }
